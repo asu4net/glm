2de10978390c6c97a16740cd26c44cbf48f37ba7
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2023-04-23 10:16:24 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "glm"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+    "glm/**.hpp",
+		"glm/**.inl"
+}
+
+filter "system:windows"
+    systemversion "latest"
+    staticruntime "on"
+    
+filter "configurations:Debug"
+    defines "NIT_DEBUG"
+    runtime "Debug"
+    symbols "on"
+    
+filter "configurations:Release"
+    defines "NIT_RELEASE"
+    runtime "Release"
+    optimize "on"
