4ad5cbd962bc233d728f44a6a5054c2ea19c6c0c
Alex <asu4net@gmail.com>
2023-04-26 19:12:15 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "glm"
     kind "StaticLib"
     language "C++"
     cppdialect "C++20"
-    staticruntime "on"
+    staticruntime "off"
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
